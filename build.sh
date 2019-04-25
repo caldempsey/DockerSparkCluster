18ef733d9093b0adc5657c689ab49ec48660cd3f
mmacheerpuppy <callum.leach@hotmail.co.uk>
2019-04-25 14:49:18 +0100

    Updated line endings to clrf

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ docker build -t spark-base:${SPARK_VERSION} ./compose/spark/base --build-arg SPA
 docker build -t spark-master:${SPARK_VERSION} ./compose/spark/master --build-arg FROM_IMAGE=spark-base:${SPARK_VERSION}
 docker build -t spark-worker:${SPARK_VERSION} ./compose/spark/worker --build-arg FROM_IMAGE=spark-base:${SPARK_VERSION}
 docker build -t spark-submit:${SPARK_VERSION} ./compose/spark/submit --build-arg FROM_IMAGE=spark-base:${SPARK_VERSION}
+
 # https://docs.docker.com/compose/reference/up/
 docker-compose up
