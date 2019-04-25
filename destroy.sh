9c95f053e24f7a4cfc4117078ec6e0ac695d2984
mmacheerpuppy <callum.leach@hotmail.co.uk>
2019-04-25 16:43:39 +0100

    I can't believe this actually works

--- destroy.sh
+++ destroy.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-chmod +x compose/spark/build-images.sh
-./compose/spark/build-images.sh
-
 # https://docs.docker.com/compose/reference/down/
 docker-compose down
 # https://docs.docker.com/compose/reference/rm/
-docker-compose rm -f -s -v 
+docker-compose rm -f -s -v
