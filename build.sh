05892715c42e42a8146ff3955a0703f4f3f83349
mmacheerpuppy <callum.leach@hotmail.co.uk>
2019-04-25 15:20:10 +0100

    Updated line endings from linux

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ set -e
 
 # Acquire environment variables for building images.
 source .env
-# Build images 
-docker build -t spark-base:${SPARK_VERSION} ./compose/spark/base --build-arg SP$
-docker build -t spark-master:${SPARK_VERSION} ./compose/spark/master --build-ar$
-docker build -t spark-worker:${SPARK_VERSION} ./compose/spark/worker --build-ar$
-docker build -t spark-submit:${SPARK_VERSION} ./compose/spark/submit --build-ar$
 
+# Build images
+docker build -t spark-base:${SPARK_VERSION} ./compose/spark/base --build-arg SPARK_VERSION=${SPARK_VERSION}
+docker build -t spark-master:${SPARK_VERSION} ./compose/spark/master --build-arg FROM_IMAGE=spark-base:${SPARK_VERSION}
+docker build -t spark-worker:${SPARK_VERSION} ./compose/spark/worker --build-arg FROM_IMAGE=spark-base:${SPARK_VERSION}
+docker build -t spark-submit:${SPARK_VERSION} ./compose/spark/submit --build-arg FROM_IMAGE=spark-base:${SPARK_VERSION}
 # https://docs.docker.com/compose/reference/up/
 docker-compose up
-
-
